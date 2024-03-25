47fb06d455d5fc854525e736540c87e4f4fc89a4
Ivan Pyatlin <piatlin2002@gmail.com>
2024-03-25 17:04:21 +0300

    добавил разрешенные состояния задач

--- src/main/java/org/example/TaskState.java
+++ src/main/java/org/example/TaskState.java
@@ -1,5 +1,31 @@
 package org.example;
 
+import java.util.Set;
+
 public enum TaskState {
-    RUNNING, SUSPENDED, READY, WAIT
+    RUNNING {
+        @Override
+        Set<TaskState> nextStates() {
+            return Set.of(SUSPENDED, READY, WAIT);
+        }
+    } , SUSPENDED {
+        @Override
+        Set<TaskState> nextStates() {
+            return Set.of(READY);
+        }
+    }, READY {
+        @Override
+        Set<TaskState> nextStates() {
+            return Set.of(RUNNING);
+        }
+    }, WAIT {
+        @Override
+        Set<TaskState> nextStates() {
+            return Set.of(READY);
+        }
+    };
+
+    Set<TaskState> nextStates() {
+        return Set.of();
+    }
 }
